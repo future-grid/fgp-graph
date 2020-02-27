926329857dfec235a266ae8c350c15210767514a
Eric Wang <flexdeviser@gmail.com>
2020-02-27 13:53:26 +1100

    [#54] draw rect to select lines on graph

---
+++ src/extras/toolbar/RectSelection.ts
@@ -0,0 +1,16 @@
+export default class RectSelection {
+
+
+    /**
+     * rect selection plugin
+     * @param parentElement parent div -> header
+     * @param onSelect  callback func
+     */
+    constructor(public parentElement: Element, public onSelect: () => Array<string>) {
+    }
+
+
+    
+
+
+}
